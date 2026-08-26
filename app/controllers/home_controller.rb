c3f1d18b590f7433cd94cb07dccefc0d39b72e94
Luis <luisarlindo@DESKTOP-0OEVF45.localdomain>
2026-08-26 03:26:25 -0300

    fix: prevent unwanted redirect to mural from home and fix toggle_online_profile_path helper

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
-﻿class HomeController < ApplicationController
+class HomeController < ApplicationController
   def index
-    if logged_in?
-      redirect_to mural_index_path
-    else
-      @user = User.new
-    end
+    @user = current_user || User.new
   end
 end
