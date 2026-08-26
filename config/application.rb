403415a793722b7c9b1606bc28cdaf9743102c23
Luis <luisarlindo@DESKTOP-0OEVF45.localdomain>
2026-08-26 02:56:28 -0300

    fix: configure Rails UTF-8 and fix character encoding mojibake across views and assets

--- config/application.rb
+++ config/application.rb
@@ -17,11 +17,9 @@ module Azarar
     config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.encoding = "utf-8"
+    config.time_zone = "Brasilia"
+    config.i18n.default_locale = :"pt-BR"
+    config.i18n.available_locales = [:"pt-BR", :en]
   end
 end
